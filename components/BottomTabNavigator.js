1458c5aff97c0e6c62a109bd172c8e45dcd319a2
Byjus5 <89545408+Byjus5@users.noreply.github.com>
2021-11-18 15:22:16 -0600

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -24,7 +24,7 @@ export default class BottomTabNavigator extends Component {
                 iconName = "time";
               }
 
-              // You can return any component that you like here!
+              // Puedes devolver cualquier componente que quieras aquí
               return (
                 <Ionicons
                   name={iconName}
@@ -54,8 +54,8 @@ export default class BottomTabNavigator extends Component {
             }
           }}
         >
-          <Tab.Screen name="Ride" component={RideScreen} />
-          <Tab.Screen name="Ride History" component={RideHistoryScreen} />
+          <Tab.Screen name="Montar" component={RideScreen} />
+          <Tab.Screen name="Historial" component={RideHistoryScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     );
